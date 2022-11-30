e9e9bcefbeb602f1cc771e32f86cbf53bfccd9df
Tec <Tec@SKAB1-PC9>
2022-11-30 08:47:47 +0100

    Add project files.

---
+++ Kat/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kat
+{
+    class Cat
+    {
+        public string Farve { get; set; }
+
+        public Cat(string _farve)
+        {
+            Farve = _farve;
+        }
+
+       
+        
+        
+            
+
+    }
+}
---
+++ Kat/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Cat cat = new("Grå");
+cat.Farve = "Gul";
+
+Console.WriteLine(cat.Farve);
